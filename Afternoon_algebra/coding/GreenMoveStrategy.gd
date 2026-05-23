0e206a64fad66cd6362e7b34f457476c6edd2534
zht507 <18904038452@163.com>
2026-05-23 09:30:08 +0800

    完成了绿球bug的修复，但因为重写逻辑导致绿球链式碰撞并没有限制在只能发生一次，个人觉得可以不用限制所以暂时没有添加，如需要则后续再完善代码（虽然感觉会导致很多bug）

--- Afternoon_algebra/coding/GreenMoveStrategy.gd
+++ Afternoon_algebra/coding/GreenMoveStrategy.gd
@@ -6,6 +6,5 @@ func execute(marble: Marble2D, direction: int, steps: int) -> bool:
 	var success = marble._move_step_by_step(direction, steps)
 	if success and marble.is_alive:
 		var push_range = marble.get("push_range") if "push_range" in marble else 1
-		GreenMarbleHelper.push_neighbors(marble, push_range)
+		var _coords = GreenMarbleHelper.push_neighbors(marble, push_range)
 	return success
-
